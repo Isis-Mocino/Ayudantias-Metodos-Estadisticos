d3840bdf1686565488c1e915db583186a830264f
Isis Mociño <83838132+Isis-Mocino@users.noreply.github.com>
2023-04-06 23:55:39 -0600

    Update 6_Tarea 3 pt 1.R

--- 6_Tarea 3 pt 1.R
+++ 6_Tarea 3 pt 1.R
@@ -1,10 +1,15 @@
+# En esta ayudantia estudiaremos la relacion entre
+# la media y varianza en una distribucion Poisson
+
 # Una muestra n = 50, theta = 3 -----------------------
 
-v1 <- rpois(100,3)
+n <- 100 # Tamano de muestra
+v1 <- rpois(n,3) # Generamos muestra
 
-theta1 <- mean(v1)
-theta2 <- var(v1)*(99/100)
+theta1 <- mean(v1) # Media
+theta2 <- var(v1)*((n-1)/n) # Varianza
 
+# Histograma con media y varianza
 hist(v1,
      main = "titulo",
      xlab = "x",
@@ -20,19 +25,23 @@ legend(x = "topright", legend = c(3, "media", "varianza"),
 
 # 500 muestras n = 100, theta = 3 ----------------------
 
-n <- 100
-t1 <- rep(0, 500)
-t2 <- rep(0, 500)
+n <- 100 # Tamano de muestra
+t1 <- rep(0, 500) # Vector de medias
+t2 <- rep(0, 500) # vector de varianzas
 
+# Generamos 500 muestras y guardamos las medias y varianzas
 for (i in 1:500) {
   m <- rpois(n, 3)
   t1[i] <- mean(m)
   t2[i] <- var(m)*((n-1)/n)
 }
 
+# Graficamos los pares de medias y varianzas obtenidos
 plot(t1, t2,
      main = "Media vs Varianza (muestral)",
      xlab = "Media muestral",
      ylab = "Varianza muestral")
 abline(v=3, col="red", lwd = 2, lty = 2)
 abline(h=3, col="red", lwd = 2, lty = 2)
+
+# Pregunta: ¿que observas?
